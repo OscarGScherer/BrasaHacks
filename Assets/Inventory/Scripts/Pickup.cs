e93713cf43d20f716e33014e2df3fca2a8c19438
diogxd <42854397+diogxd@users.noreply.github.com>
2021-04-02 23:42:50 -0300

    inventario 2
    aaaaaaaaaaaaaaaa

--- Assets/Inventory/Scripts/Pickup.cs
+++ Assets/Inventory/Scripts/Pickup.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Pickup : MonoBehaviour{
     private Inventory inventory;
+    public Tile block;
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
@@ -12,12 +14,17 @@ public class Pickup : MonoBehaviour{
     {
      if (other.CompareTag("Player"))
         {
-            for (int i = 0; i< inventory.slots.Length; i++)
+            for (int i = 0; i< inventory.slot.Length; i++)
             {
-                if (inventory.isFull[i]== false)
+                if (inventory.slot[i] == null)
                 {
-                    inventory.isFull[i] = true;
+                    inventory.slot[i] = block;
+                    inventory.amount[i] = 1;
                     break;
+                }
+                else if (inventory.slot[i]== block)
+                {
+                    inventory.amount[i] += 1;
                 }
             }
         }  
